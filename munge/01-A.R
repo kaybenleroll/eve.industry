0d7fa2d5a6140838352f02e1bf75f6a76d4c862f
Mick Cooney <mcooney@kaybensoft.com>
2013-05-22 00:33:23 +0100

    Added the data files and the idlist for the marketdata

--- munge/01-A.R
+++ munge/01-A.R
@@ -4,3 +4,21 @@ static.dbconnection <- data.connection;
 
 item.dt <- get.item.data();
 name.dt <- get.name.data();
+
+market.item.dt <- item.dt[!is.na(marketGroupID)];
+
+idlist.asteroid <- market.item.dt[categoryName == 'Asteroid']$typeID;
+idlist.material <- market.item.dt[categoryName == 'Material']$typeID;
+idlist.pi       <- market.item.dt[categoryName == 'Planetary Commodities']$typeID;
+idlist.ship     <- market.item.dt[categoryName == 'Ship']$typeID;
+
+idlist.gas      <- market.item.dt[groupName == 'Harvestable Cloud']$typeID;
+idlist.posfuel  <- market.item.dt[groupName == 'Fuel Block']$typeID;
+
+
+idlist.t2mods   <- construction.t2mods$typeID;
+
+idlist.marketdata <- c(idlist.asteroid, idlist.material, idlist.pi, idlist.ship, idlist.gas, idlist.t2mods);
+
+idlist.buyitems   <- c(idlist.asteroid, idlist.pi, idlist.gas);
+idlist.sellitems  <- c(idlist.posfuel);
