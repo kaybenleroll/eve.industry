240573d49ee09d76bd019ff527b0290668678183
Mick Cooney <mickcooney@gmail.com>
2013-06-24 14:27:16 +0100

    Adding data files up to 2013-06-23

--- munge/01-A.R
+++ munge/01-A.R
@@ -7,18 +7,27 @@ name.dt <- get.name.data();
 
 market.item.dt <- item.dt[!is.na(marketGroupID)];
 
-idlist.asteroid <- market.item.dt[categoryName == 'Asteroid']$typeID;
-idlist.material <- market.item.dt[categoryName == 'Material']$typeID;
-idlist.pi       <- market.item.dt[categoryName == 'Planetary Commodities']$typeID;
-idlist.ship     <- market.item.dt[categoryName == 'Ship']$typeID;
+idlist.asteroid   <- market.item.dt[categoryName == 'Asteroid']$typeID;
+idlist.material   <- market.item.dt[categoryName == 'Material']$typeID;
+idlist.pi         <- market.item.dt[categoryName == 'Planetary Commodities']$typeID;
+idlist.ship       <- market.item.dt[categoryName == 'Ship']$typeID;
+idlist.commodity  <- market.item.dt[categoryName == 'Commodity']$typeID;
 
-idlist.gas      <- market.item.dt[groupName == 'Harvestable Cloud']$typeID;
-idlist.posfuel  <- market.item.dt[groupName == 'Fuel Block']$typeID;
+idlist.gas        <- market.item.dt[groupName == 'Harvestable Cloud']$typeID;
+idlist.posfuel    <- market.item.dt[groupName == 'Fuel Block']$typeID;
+idlist.datacore   <- market.item.dt[groupName == 'Datacores']$typeID;
+idlist.decryptor  <- market.item.dt[grep('Decryptor', groupName)]$typeID;
+
 
 
 idlist.t2mods   <- construction.t2mods$typeID;
 
-idlist.marketdata <- c(idlist.asteroid, idlist.material, idlist.pi, idlist.ship, idlist.gas, idlist.t2mods);
+idlist.marketdata <- c(idlist.asteroid, idlist.material, idlist.pi, idlist.ship, idlist.gas, idlist.t2mods, idlist.datacore, idlist.decryptor, idlist.commodity);
 
-idlist.buyitems   <- c(idlist.asteroid, idlist.material, idlist.pi, idlist.gas);
+idlist.buyitems   <- c(idlist.asteroid, idlist.material, idlist.pi, idlist.gas, idlist.datacore, idlist.decryptor, idlist.commodity);
 idlist.sellitems  <- c(idlist.posfuel);
+
+
+idlist.marketdata <- unique(idlist.marketdata);
+idlist.buyitems   <- unique(idlist.buyitems);
+idlist.sellitems  <- unique(idlist.sellitems);
